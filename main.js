6fd3afc865a1064c28789761af6854c470f5ecf8
phenyyyyyy <152913059+phenyyyyyy@users.noreply.github.com>
2023-12-25 00:24:16 +0800

    Update main.js
    在形状类，球类中完成了球形实例，计算机能给我画多个圆

--- main.js
+++ main.js
@@ -1,46 +1,76 @@
 // 设置画布
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
-var ball = {
-  x: 200,
-  y: 200,
-  vx: 5,
-  vy: 2,
-  radius: 25,
-  draw: function (color) {
-    ctx.beginPath();
-    ctx.strokeStyle = "white";
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fillStyle = color;
-    ctx.fill();
-  },
-};
-
-
-function runBall() {
-  ctx.fillStyle = 'rgba(0,0,0,0.2)';
-  ctx.fillRect(0,0,width,height);
-  ball.draw("pink");
-  ball.x += ball.vx;
-  ball.y += ball.vy;
-  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-    ball.vy = -ball.vy;
+class Shape {
+  x;
+  y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-    ball.vx = -ball.vx;
+  introduceShape() {
+    console.log(` this position is: (${this.x}, ${this.y} )`);
   }
-  window.requestAnimationFrame(runBall);
 }
-window.requestAnimationFrame(runBall);
+
+class Ball extends Shape {
+  radious;color;
+  constructor(x,y,radious,color){
+    super(x,y);
+    this.radious = radious;
+    this.color =color;
+  }
+  darw(){
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.radious,0,Math.PI*2,);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill()
+  }
+  introduceShape() {
+    console.log(
+      ` this position is: (${this.x}, ${this.y} )`,
+    );
+  }
+
+}
+
+const shape = new Shape("60", "100");
+shape.introduceShape();
+const ballarr = [];
+
+for (i=0;i<5;i++){
+ballarr[i] += "ball" +i
+console.log(i,ballarr[i])
+}
+
+ballarr.forEach(element => {
+  element = new Ball(random(10,1000),random(10,1000),random(10,80),randomColor());
+  console.log(element)
+  element.darw()
+});
+
+
 
 // 生成随机数的函数
 
-function random(min,max) {
+function random(min, max) {
   const num = Math.floor(Math.random() * (max - min)) + min;
   return num;
+}
+
+//生成随机颜色
+function randomColor() {
+  return (
+    "rgb(" +
+    random(0, 255) +
+    ", " +
+    random(0, 255) +
+    ", " +
+    random(0, 255) +
+    ")"
+  );
 }
